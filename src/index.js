f6e0f295389d51e191348e3a3e378948f8f14e39
Oleksandr Shyian <shyian.code@gmail.com>
2023-12-05 04:48:29 +0200

    add styles and sripts

--- src/index.js
+++ src/index.js
@@ -6,135 +6,40 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 import { fetchBreeds, fetchCatByBreed } from "./js/cat-api.js";
 
-const breedSelect = document.querySelector('.breed-select');
-const catInfoDiv = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
-const errorDiv = document.querySelector('.error');
+const catInfoDiv = document.querySelector(".cat-info");
+const loader = document.querySelector(".loader");
+const error = document.querySelector(".error");
+const breedSelect = document.querySelector(".breed-select");
 
-
-// const slimSelect = new SlimSelect({
-//   select: breedSelect,
-//   settings: {
-//     placeholderText: 'Search breeds',
-//   },
-// });
-
-// використання iziToast
-iziToast.success({
-  title: "Success",
-  message: "Operation completed successfully",
-});
-
-
-function showLoader() {
-  loader.classList.add("visible");
-  breedSelect.classList.add("hidden");
-  catInfoDiv.classList.add("hidden"); 
-}
-
-function hideBreedSelect() {
-  breedSelect.classList.add("hidden");
-}
-
-function hideLoader() {
-  loader.classList.remove("visible");
-}
-
-function showBreedSelect() {
-  breedSelect.classList.remove("hidden");
-}
-
-function hideCatInfo() {
-  catInfoDiv.classList.add("hidden");
-}
-
-function showCatInfo() {
-  catInfoDiv.classList.remove("hidden");
-}
-
-function populateBreedSelect(breeds) {
-  breedSelect.innerHTML = "";
-  breeds.forEach(breed => {
-    const option = document.createElement("option");
-    option.value = breed.id;
-    option.textContent = breed.name;
-    breedSelect.appendChild(option);
-  });
-}
-
-function displayCatInfo(cat, origin, country_code, noImage) {
-  const img = document.createElement("img");
-  img.src = cat[0].url;
-  img.alt = `${cat[0].breeds[0].name}`;
-
-  const breedName = document.createElement("p");
-  breedName.textContent = `Breed: ${cat[0].breeds[0].name}`;
-
-  const description = document.createElement("p");
-  description.textContent = `Description: ${cat[0].breeds[0].description}`;
-
-  const temperament = document.createElement("p");
-  temperament.textContent = `Temperament: ${cat[0].breeds[0].temperament}`;
-
-  const countryImg = document.createElement("img");
-  countryImg.src = `https://flagsapi.com/${country_code}/shiny/64.png`;
-  countryImg.alt = `${origin}`;
-  countryImg.onerror = () => {
-    countryImg.src = noImage;
-  };
-  countryImg.style.width = "64px";
-
-  const wrapperDiv = document.createElement("div");
-  wrapperDiv.classList.add("wrapper");
-  wrapperDiv.innerHTML = `
-    <img class="cat-img" width="400" src="${img.src}" alt="${img.alt}">
-    <div class="info-wrap">
-      <h2 class="info-title">${breedName.textContent}</h2>
-      <p><b class="primary">Description:</b> ${description.textContent}</p>
-      <p><b class="primary">Temperament:</b> ${temperament.textContent}</p>
-      <div>
-        <p><b class="primary">Country:</b> ${origin}</p>
-        <img class="cat-img" width="400" src="${countryImg.src}" alt="${countryImg.alt}">
-      </div>
-    </div>
-  `;
-
-  wrapperDiv.appendChild(countryImg);
-  catInfoDiv.innerHTML = "";
-  catInfoDiv.appendChild(wrapperDiv);
-}
-
-
-// Fetch breeds on page load
-showLoader();
-fetchBreeds()
-  .then(breeds => {
-    hideLoader();
-    populateBreedSelect(breeds);
-    showBreedSelect(); // Показати select.breed-select
-  })
-  .catch(() => {
-    hideLoader();
-    showError();
-  });
-
-// Handle breed select change
-breedSelect.addEventListener("change", () => {
+breedSelect.addEventListener("change", async () => {
   const selectedBreedId = breedSelect.value;
-
-  if (selectedBreedId) {
-    showLoader();
-    hideError();
-    hideBreedSelect(); // Приховати select.breed-select
-
-    fetchCatByBreed(selectedBreedId)
-      .then(cat => {
-        hideLoader();
-        displayCatInfo(cat);
-      })
-      .catch(() => {
-        hideLoader();
-        showError();
-      });
+  try {
+    loader.style.display = "block";
+    catInfoDiv.style.display = "none";
+    const catData = await fetchCatByBreed(selectedBreedId);
+    // Відобразити інформацію про кота
+    loader.style.display = "none";
+    catInfoDiv.style.display = "block";
+  } catch (error) {
+    loader.style.display = "none";
+    error.style.display = "block";
+    console.error("Помилка отримання інформації про кота:", error);
   }
 });
+
+
+const populateBreeds = async () => {
+  try {
+    const breeds = await fetchBreeds();
+    breeds.forEach((breed) => {
+      const option = document.createElement("option");
+      option.value = breed.id;
+      option.text = breed.name;
+      breedSelect.appendChild(option);
+    });
+  } catch (error) {
+    console.error("Помилка отримання порід:", error);
+  }
+};
+
+populateBreeds();
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,20 +3,22 @@ import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_8dopgEoZqjLeDX5BcU4nL2k4qRgKONIXj7FAVN0qokHbLiZxLlebiy9Al26Wgzgs";
 
-export function fetchBreeds() {
-  return axios.get("https://api.thecatapi.com/v1/breeds")
-    .then(response => response.data)
-    .catch(error => {
-      console.error("Error fetching breeds:", error);
-      throw error;
-    });
-}
+export const fetchBreeds = async () => {
+  try {
+    const response = await axios.get("https://api.thecatapi.com/v1/breeds");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export function fetchCatByBreed(breedId) {
-  return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(response => response.data)
-    .catch(error => {
-      console.error("Error fetching cat by breed:", error);
-      throw error;
-    });
-}
+export const fetchCatByBreed = async (breedId) => {
+  try {
+    const response = await axios.get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
