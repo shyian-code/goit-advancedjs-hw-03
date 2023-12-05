88c7f4777b4b6db89218d7810888eb7b27f7a03a
Oleksandr Shyian <shyian.code@gmail.com>
2023-12-05 04:54:49 +0200

    add styles and sripts

--- src/index.js
+++ src/index.js
@@ -5,30 +5,13 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { fetchBreeds, fetchCatByBreed } from "./js/cat-api.js";
-
+const breedSelect = document.querySelector(".breed-select");
 const catInfoDiv = document.querySelector(".cat-info");
 const loader = document.querySelector(".loader");
 const error = document.querySelector(".error");
-const breedSelect = document.querySelector(".breed-select");
 
-breedSelect.addEventListener("change", async () => {
-  const selectedBreedId = breedSelect.value;
-  try {
-    loader.style.display = "block";
-    catInfoDiv.style.display = "none";
-    const catData = await fetchCatByBreed(selectedBreedId);
-    // Відобразити інформацію про кота
-    loader.style.display = "none";
-    catInfoDiv.style.display = "block";
-  } catch (error) {
-    loader.style.display = "none";
-    error.style.display = "block";
-    console.error("Помилка отримання інформації про кота:", error);
-  }
-});
-
-
-const populateBreeds = async () => {
+// Заповнюємо селект порід при завантаженні сторінки
+document.addEventListener("DOMContentLoaded", async () => {
   try {
     const breeds = await fetchBreeds();
     breeds.forEach((breed) => {
@@ -40,6 +23,43 @@ const populateBreeds = async () => {
   } catch (error) {
     console.error("Помилка отримання порід:", error);
   }
-};
+});
 
-populateBreeds();
+// Обробка події вибору породи
+breedSelect.addEventListener("change", async () => {
+  const selectedBreedId = breedSelect.value;
+  try {
+    showLoader();
+    hideError();
+    const catData = await fetchCatByBreed(selectedBreedId);
+    displayCatInfo(catData);
+  } catch (error) {
+    hideLoader();
+    showError();
+    console.error("Помилка отримання інформації про кота:", error);
+  }
+});
+
+// Функція для відображення інформації про кота
+const displayCatInfo = (catData) => {
+  const cat = catData[0];
+  const image = cat.url;
+  const breedName = cat.breeds[0].name;
+  const description = cat.breeds[0].description;
+  const temperament = cat.breeds[0].temperament;
+
+  // Відображаємо зображення та інформацію про кота
+  const catImage = document.createElement("img");
+  catImage.src = image;
+  catInfoDiv.innerHTML = `
+    <div>
+      <h3>${breedName}</h3>
+      <p><strong>Description:</strong> ${description}</p>
+      <p><strong>Temperament:</strong> ${temperament}</p>
+    </div>
+  `;
+  catInfoDiv.appendChild(catImage);
+
+  hideLoader();
+  catInfoDiv.style.display = "block";
+};
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -5,20 +5,46 @@ axios.defaults.headers.common["x-api-key"] = "live_8dopgEoZqjLeDX5BcU4nL2k4qRgKO
 
 export const fetchBreeds = async () => {
   try {
+    showLoader();
+    hideError();
     const response = await axios.get("https://api.thecatapi.com/v1/breeds");
+    hideLoader();
     return response.data;
   } catch (error) {
+    hideLoader();
+    showError();
     throw error;
   }
 };
 
 export const fetchCatByBreed = async (breedId) => {
   try {
+    showLoader();
+    hideError();
     const response = await axios.get(
       `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
     );
+    hideLoader();
     return response.data;
   } catch (error) {
+    hideLoader();
+    showError();
     throw error;
   }
+};
+
+const showLoader = () => {
+  document.querySelector(".loader").style.display = "block";
+};
+
+const hideLoader = () => {
+  document.querySelector(".loader").style.display = "none";
+};
+
+const showError = () => {
+  document.querySelector(".error").style.display = "block";
+};
+
+const hideError = () => {
+  document.querySelector(".error").style.display = "none";
 };
